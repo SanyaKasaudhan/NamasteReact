5100bbcdf7f160b9bcdffd605188d3f83e0f2d35
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-01-17 08:54:36 +0530

    body

--- React Day5/src/components/Content.js
+++ React Day5/src/components/Content.js
@@ -13,8 +13,31 @@ export const Content = () => {
       <button type="button" onClick={searchRestaurant}>
         Search
       </button>
+    <Body />
+      
       {restaurantList[0].data?.name}
       {/* <Restaurants restaurant={restaurantList[0]} /> */}
     </>
+  );
+};
+
+const RestaurantCard = ({name,parentId,address}) => {
+  return (
+    <div className="card">
+      <h2>{name}</h2>
+      <h2>{parentId}</h2>
+      <h2>{address}</h2>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="restaurant-list">
+      {
+        restaurantList.map( e=>
+          <RestaurantCard key={e.data.id} {...e.data} />     
+          )
+      }
+    </div>
   );
 };
