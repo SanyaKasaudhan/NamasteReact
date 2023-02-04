fdd8617ff2a3e9b3d717448d739bf5e3272221fa
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-02-04 19:53:13 +0530

    State lifting

--- React Day 11/App.js
+++ React Day 11/App.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-const App = () => {
+import "./index.css"
+const App = () => { 
+   const [isVisible, setIsVisible]=useState(true);
+   
   return (
-    <>
-      <h1>
-        Hi
-      </h1>
+   <>
+      <div className="container">
+       {/* { isVisible ? <button onClick={setIsVisible(false)}>Show</button> : <button onClick={setIsVisible(true)}>Hide</button>}  */}
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+      </div>
     </>
   );
 };
