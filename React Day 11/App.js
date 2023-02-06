d6a895b181787703406fa8b10909dde40692cf77
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-02-06 09:37:22 +0530

    checking with api data

--- React Day 11/App.js
+++ React Day 11/App.js
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"
+
+const Section = (description)=>{
+  return(
+    <>
+
+    </>
+  )
+}
+
 const App = () => { 
    const [isVisible, setIsVisible]=useState(true);
    
   return (
    <>
-   <Career description={Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum }
+   <Section description={"Career Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}
+   isVisible/>
+   <Section description={"About Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}
+    />
+   <Section description={"Panel Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}
    />
-   <About  />
-   <Panel />
       {/* <div className="container">
-       { isVisible ? <button onClick={setIsVisible(false)}>Show</button> : <button onClick={setIsVisible(true)}>Hide</button>} 
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
+         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
       </div> */}
     </>
   );
--- React Day7/src/components/Restaurants.js
+++ React Day7/src/components/Restaurants.js
@@ -1,6 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { restaurantList } from "../constants";
 import RestaurantCard from "./RestaurantCard";
+
+const FOOD_API="https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+const IMG_CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+ const FETCH_MENU_URL =
+"https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId=";
 
 const filterData = (searchText, allRestaurants) => {
   return allRestaurants.filter((restaurant) =>
@@ -10,6 +16,29 @@ const filterData = (searchText, allRestaurants) => {
 };
 
 const Restaurants = () => {
+
+
+  const [allRestaurant, setAllRestaurant] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+  useEffect(() => {
+    getRestaurants();
+  }, []);
+
+  async function getRestaurants() {
+    // handle the error using try... catch
+    try {
+      const data = await fetch(FOOD_API);
+      const json = await data.json();
+      // updated state variable restaurants with Swiggy API data
+      setAllRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+      console.log("set",json)
+      setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   const [searchText, setSearchText] = useState();
   const [restaurants, setRestaurants] = useState(restaurantList);
   const [allRestaurants, setAllRestaurants]=useState(restaurantList);
