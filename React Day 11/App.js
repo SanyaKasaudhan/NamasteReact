854f5dddf8ea0596f734caf7cd85dd611a170370
SANYA KASAUDHAN <kasaudhansanya@gmail.com>
2023-02-06 14:11:48 +0530

    lifting state

--- React Day 11/App.js
+++ React Day 11/App.js
@@ -2,28 +2,43 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css"
 
-const Section = (description)=>{
+const Section = ({title, description ,isVisible,setIsVisible})=>{
+  
   return(
     <>
-
+    <div className="container">
+      <h1>{title}</h1> 
+      {isVisible ? <><button onClick={()=>setIsVisible(true)}>Hide</button> <p>{description}</p></> : 
+      <button onClick={()=>setIsVisible(false)}>Show</button>}   
+      </div>
     </>
   )
 }
 
 const App = () => { 
-   const [isVisible, setIsVisible]=useState(true);
-   
+  const [visibleSection, setIsVisbleSection]=useState("about12345");
+ 
   return (
    <>
    <Section description={"Career Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}
-   isVisible/>
+   title={"About"}
+   isVisible={visibleSection==="about12345"}
+   setIsVisible={()=> setIsVisbleSection("about12345")}/>
    <Section description={"About Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}
-    />
+   title={"Career"} 
+   isVisible={visibleSection==="career"}
+   setIsVisible={()=>setIsVisbleSection("career")}/>
    <Section description={"Panel Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"}
-   />
-      {/* <div className="container">
-         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum</p>
-      </div> */}
+   title={"Panel"}
+   isVisible={visibleSection==="panel"}
+   setIsVisible={(eve)=> 
+    {
+   if(eve)
+   {setIsVisbleSection("panel")
+   }else{
+    setIsVisbleSection(" ")
+   }}
+  }/>      
     </>
   );
 };
